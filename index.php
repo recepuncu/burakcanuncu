bd043123ec69a0e0a9c1f7a5ebfd6e63b1b2861b
RECEP UNCU <recepuncu@gmail.com>
2016-07-04 18:05:35 +0300

    Add files via upload
    initial

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Burak Can Uncu</title>
+	<link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://s.mlcdn.co/animate.css">
+	<style type="text/css">
+		body {
+			overflow: hidden;
+		}
+		h1.caption {
+			font-family: 'PT Sans Narrow', sans-serif;
+			size: 28px;
+			/*color: darkslategrey;*/
+		}
+		#parent { 
+			position: absolute;
+			display: table;
+			width: 100%;
+			height: 100%;
+			text-align: center;
+		}
+		#child {
+			display: table-cell;
+			vertical-align: middle;
+		}
+		.color {
+			color: rgb(255,87,34); /* Old browsers */
+			color: -moz-linear-gradient(top, rgba(255,87,34,1) 0%, rgba(233,30,99,1) 100%); /* FF3.6-15 */
+			color: -webkit-linear-gradient(top, rgba(255,87,34,1) 0%,rgba(233,30,99,1) 100%); /* Chrome10-25,Safari5.1-6 */
+			color: linear-gradient(to bottom, rgba(255,87,34,1) 0%,rgba(233,30,99,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+			filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ff5722', endColorstr='#e91e63',GradientType=0 ); /* IE6-9 */		
+		}
+	</style>
+  </head>
+  <body>
+	<div id="parent">
+		<div id="child">
+			<h1 class="caption color animated fadeInDown"> BurakCanUncu </h1>
+		</div>
+	</div>
+  </body>
+</html>
